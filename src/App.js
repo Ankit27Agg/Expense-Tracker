7511d6219bed52dce7dc57d7aba005c755dc0120
Ankit27Agg <worltresting@gmail.com>
2022-01-25 16:42:34 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@ import React, {useState} from "react";
 import NewExpense from "./Components/New Expense/NewExpense";
 
 let dummy_items = [
-  {
-    id: 1,
-    title: 'School',
-    amount: 2000 ,
-    date: new Date(2022, 1, 12) //don't write '0'(zero)before any number in months //and indexing for months is 0-11
-  },
-  {
-    id: 2,
-    title: 'college',
-    date: new Date(2021, 4, 22),
-    amount: 3000 
-  },
-  {
-    id: 3,
-    title: 'House Rent',
-    date: new Date(2019, 1, 12),
-    amount: 40000 
-  },
-  {
-    id: 4,
-    title: 'Grocery',
-    date: new Date(2022, 11, 11),
-    amount: 5000 
-  }
+  // {
+  //   id: 1,
+  //   title: 'School',
+  //   amount: 2000 ,
+  //   date: new Date(2022, 1, 12) //don't write '0'(zero)before any number in months //and indexing for months is 0-11
+  // },
+  // {
+  //   id: 2,
+  //   title: 'college',
+  //   date: new Date(2021, 4, 22),
+  //   amount: 3000 
+  // },
+  // {
+  //   id: 3,
+  //   title: 'House Rent',
+  //   date: new Date(2019, 1, 12),
+  //   amount: 40000 
+  // },
+  // {
+  //   id: 4,
+  //   title: 'Grocery',
+  //   date: new Date(2022, 11, 11),
+  //   amount: 5000 
+  // }
 ]
 
 
